2d34c6451f54cb9ca206977879ac0384d1b04739
luizoti <luizlazoti@hotmail.com>
2021-05-26 12:05:24 -0300

    fix: change variable name

--- resources/lib/utils.py
+++ resources/lib/utils.py
@@ -640,7 +640,7 @@ def load_directory_items(progressdialog, dir_path, recursive=False,
         listofitems = list(list_reorder(
             list(skip_filter(results)
                 ), showtitle=showtitle, year=year, sync_type=sync_type))
-    except KeyError:
+    except (KeyError, TypeError):
         listofitems = []
     if not allow_directories:
         for item in listofitems:
